8d4a697fc9b0fb467c8093e281a3f6701192fdb0
Mogoson <mogoson@qq.com>
2017-10-07 19:42:12 +0800

    Modify MeshExtension.cs.

--- Assets/MGS-Mesh/Scripts/Extension/MeshExtension.cs
+++ Assets/MGS-Mesh/Scripts/Extension/MeshExtension.cs
@@ -8,13 +8,13 @@
  *    Ignore.
  *  Class List:
  *    <ID>           <name>             <description>
- *     1.         MeshExtension            Ignore.
+ *     1.         MeshCombiner             Ignore.
  *  Function List:
  *    <class ID>     <name>             <description>
  *     1.
  *  History:
  *    <ID>    <author>      <time>      <version>      <description>
- *     1.     Mogoson     5/24/2017       0.1.0        Create this file.
+ *     1.     Mogoson     5/24/2017       0.1.0       Create this file.
  *************************************************************************/
 
 using System.Collections.Generic;
